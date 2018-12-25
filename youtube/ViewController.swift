d9bf0ae24ec94f1ab073f7f907dd9b534f1a5059
developer-ios <s.sachuk@xpay.com.ua>
2018-12-25 09:04:52 +0200

    Initial Commit

---
+++ youtube/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube
+//
+//  Created by developer-ios on 12/25/18.
+//  Copyright © 2018 devrock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+
+}
+
